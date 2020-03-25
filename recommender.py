9e881bd3118c7b49cac871f56cbee81678ee095c
thealphadollar <thealphadollar>
2020-03-26 02:33:33 +0530

    update and finish recommender

--- recommender.py
+++ recommender.py
@@ -13,11 +13,15 @@ def get_recommendations(id):
     recommended_orders = orders[orders.order_id.isin(product_orders)]
     recommended_orders_without_initial = recommended_orders[recommended_orders.product_id!=id]
 
-    num_times_purchased = recommended_orders_without_initial.groupby("product_id")
-    # get top three of the bought products with the above specified
-    top_three = num_times_purchased.sum().sort_values(ascending=False).head(3)
+    num_instance_by_accompanying_product = recommended_orders_without_initial.groupby("product_id")["product_id"].count().reset_index(name="instances")
+    
+    num_orders_for_product = product_orders.size
+    product_instances = pd.DataFrame(num_instance_by_accompanying_product)
+    product_instances["frequency"] = product_instances["instances"]/num_orders_for_product
+    
+    recommended_products = pd.DataFrame(product_instances.sort_values("frequency", ascending=False).head(3))
 
     products = pd.read_csv("data/Product.csv")
-    recommended_products = pd.merge(top_three, products, on="product_id")
+    recommended_products = pd.merge(recommended_products, products, on="product_id")
 
-    return recommended_orders.to_json(orient="table")
+    return recommended_products.to_json(orient="table")
